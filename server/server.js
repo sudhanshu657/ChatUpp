4d9110e37f9e261b6459393500e3b3eb3fd8a078
sudhanshu singh <oberoisudhanshu215@gmail.com>
2025-12-29 18:15:56 +0530

    Fix static file path and socket.io setup

--- server/server.js
+++ server/server.js
@@ -1,41 +1,28 @@
-import express from 'express';
-import http from 'http';
-import path from 'path';
-//when i want to use socket io
-
-
-import { Server } from 'socket.io';
+import express from "express";
+import http from "http";
+import path from "path";
+import { Server } from "socket.io";
 
 const app = express();
-
 const server = http.createServer(app);
-
-//using socket io
 const io = new Server(server);
 
-//now creat a server
-
-
-
+// Serve static files correctly
+app.use(express.static(path.resolve("public")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("public/server.html"));
 });
 
-//websocket connection
 io.on("connection", (socket) => {
-    console.log("New user connection", socket.id);
-    // how to receive msg from frontemed
+    console.log("New user connected:", socket.id);
+
     socket.on("messageFromClient", (msg) => {
-        console.log(msg);
-
-        // sending msg to all clients
-        io.emit("messageFromServer", ` ${msg}`);
-
-    })
-})
+        io.emit("messageFromServer", msg);
+    });
+});
 
 const PORT = 8000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
